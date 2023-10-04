c950d3894ee9dd340532c99ebcc36fc792561ce8
Nkemjika Obi <nkemjikaobi@gmail.com>
2023-10-04 12:05:48 -0400

    Map Complete

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [region, setRegion] = useState({
+		latitude: 51.5079145,
+		longitude: -0.0899163,
+		latitudeDelta: 0.01,
+		longitudeDelta: 0.01,
+	});
+	// const tokyoRegion = {
+	// 	latitude: 35.6762,
+	// 	longitude: 139.6503,
+	// 	latitudeDelta: 0.01,
+	// 	longitudeDelta: 0.01,
+	// };
+
+	return (
+		<View style={styles.container}>
+			<MapView
+				style={styles.map}
+				initialRegion={{
+					latitude: 37.78825,
+					longitude: -122.4324,
+					latitudeDelta: 0.0922,
+					longitudeDelta: 0.0421,
+				}}
+				//onRegionChangeComplete runs when the user stops dragging MapView
+				onRegionChangeComplete={region => setRegion(region)}
+				// initialRegion={tokyoRegion}
+			>
+				<Marker coordinate={region} />
+			</MapView>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		...StyleSheet.absoluteFillObject,
+		flex: 1, //the container will fill the whole screen.
+		justifyContent: 'flex-end',
+		alignItems: 'center',
+	},
+	map: {
+		...StyleSheet.absoluteFillObject,
+	},
 });
